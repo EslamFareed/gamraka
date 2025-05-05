7942715c4aaefa97a4975543984c93cb5456d84c
Eslam Fareed <eslammando778899@gmail.com>
2025-05-05 03:32:30 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/gamraka/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gamraka
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:gamraka/firebase_options.dart';
+
+import 'core/cache_helper.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await CacheHelper.init();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false);
+  }
+}
